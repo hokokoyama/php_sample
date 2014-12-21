8a3af659695bccb03722e588ae03c7de58f9b2f1
hokoyama <hokoyama@mail>
2014-12-21 21:35:15 +0900

    12/21

---
+++ chap2/post1.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>スーパーグローバル変数</title>
+</head>
+<body>
+	<from method = "POST" action = "post2.php">
+		名前：<br/>
+		<input type="checkbox" name="food[]" value="orange" />みかん
+		<input type="checkbox" name="food[]" value="apple" />りんご
+		<input type="checkbox" name="food[]" value="strawberry" />いちご
+		<input type="submit" value="送信" />
+	</form>
+</body>
+</html>
